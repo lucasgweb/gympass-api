a752d2016d6997cce28ad9029ba37b39bc098d8e
Lucas Godoy <lucasgg.tech@gmail.com>
2023-09-02 16:11:32 -0300

    feat: Configuração do prisma e criação dos medels

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app';
 import { env } from './env';
 
 app.listen({
-    host: '0.0.0.0',
-    port: env.PORT
+  host: '0.0.0.0',
+  port: env.PORT
 }).then(()=> {
-    console.log('HTTP Server is Running!');
+  console.log('HTTP Server is Running!');
 });
